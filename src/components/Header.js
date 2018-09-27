5d25dacb9df2f3db1f7fbc669b04d3028f9a7444
Wolfgang  HellicksonSabelhaus <wolfgang_hs@Wolfgangs-MacBook-Pro.local>
2018-09-26 19:44:49 -0700

    materialUiLibrary

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 const Header = () => (
   <header>
     <h1>Hello</h1>
 
-    <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
-    <NavLink to="/bulletin" activeClassName="is-active" exact={true}>Bulletin</NavLink>
-    <NavLink to="/board" activeClassName="is-active" exact={true}>Board</NavLink>
+    <AppBar position="static">
+      <Toolbar>
+      <NavLink to="/" style={{textDecoration: 'none'}}><Button color="secondary">Home</Button></NavLink>
+      <NavLink to="/bulletin" style={{textDecoration: 'none'}}><Button color="secondary">Bulletin</Button></NavLink>
+      <NavLink to="/board" style={{textDecoration: 'none'}}><Button color="secondary">Board</Button></NavLink>
+      </Toolbar>
+    </AppBar>
+
+    {/* <NavLink to="/">Home</NavLink>
+    <NavLink to="/bulletin">Bulletin</NavLink>
+    <NavLink to="/board">Board</NavLink> */}
   </header>
 )
 
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
+import { render } from 'react-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
 
 import Header from '../components/Header.js'
 import HomePage from '../components/HomePage.js'
@@ -7,20 +10,35 @@ import BulletinPage from '../components/BulletinPage.js'
 import BoardPage from '../components/BoardPage.js'
 import NotFoundPage from '../components/NotFoundPage.js'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: blue[500]
+    },
+    secondary: {
+      main: '#ffffff'
+    },
+    typography: {
+      fontSize: 16,
+      htmlFontSize: 16
+    }
+  }
+});
 
 const AppRouter = () => (
-  <BrowserRouter>
-    <div>
-      <Header/>
-
-      <Switch>
-        <Route path="/" component={HomePage} exact={true}/>
-        <Route path="/bulletin" component={BulletinPage} exact={true}/>
-        <Route path="/board" component={BoardPage} exact={true}/>
-        <Route component={NotFoundPage}/>
-      </Switch>
-    </div>
-  </BrowserRouter>
+  <MuiThemeProvider theme={theme}>
+    <BrowserRouter>
+      <div>
+        <Header/>
+        <Switch>
+          <Route path="/" component={HomePage} exact={true}/>
+          <Route path="/bulletin" component={BulletinPage} exact={true}/>
+          <Route path="/board" component={BoardPage} exact={true}/>
+          <Route component={NotFoundPage}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </MuiThemeProvider>
 )
 
 export default AppRouter
