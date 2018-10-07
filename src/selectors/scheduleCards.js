e03d8e184567f30d90708780389bcb738efe2bb4
DanielKRing1 <DanielKRing1@gmail.com>
2018-10-07 01:43:50 -0700

    Corrected to only display Events Today/In the Future

--- src/selectors/scheduleCards.js
+++ src/selectors/scheduleCards.js
@@ -2,7 +2,7 @@ import moment from 'moment'
 
 export default (scheduleCards) => {
   let schedule = scheduleCards
-                  .filter((card) => moment(card.date, "Day").isSameOrAfter(moment("Day")))
+                  .filter((card) => card.date >= moment().startOf('day').valueOf())
                   .sort((a, b) => a.date <= b.date ? -1 : 1)
 
   return schedule
