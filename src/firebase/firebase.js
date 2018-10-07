c5dfb62ae5b7e831ba5c96772539d9618b105a58
DanielKRing1 <DanielKRing1@gmail.com>
2018-10-07 00:57:50 -0700

    Inits Firebase and Exports firebase database

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+// USED TO INCLUDE FIREBASE IN PROJECT
+// ONLY EXPORTS, TO BE USED IN OTHER FILES
+
+import * as firebase from 'firebase'
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyCRjtHrhzwYviBvndmjDQ5YvcGjj68Ye38",
+  authDomain: "acm-website-2018.firebaseapp.com",
+  databaseURL: "https://acm-website-2018.firebaseio.com",
+  projectId: "acm-website-2018",
+  storageBucket: "acm-website-2018.appspot.com",
+  messagingSenderId: "344456985029"
+};
+firebase.initializeApp(config);
+
+const database = firebase.database()
+
+export { firebase, database as default }
