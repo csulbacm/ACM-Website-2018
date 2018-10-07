21bb5beec1d0dbfacac7cb48d90d2259dcb76620
DanielKRing1 <DanielKRing1@gmail.com>
2018-10-07 00:48:53 -0700

    Added 'React-Redux' to provide Redux store to all child components

--- src/app.js
+++ src/app.js
@@ -1,8 +1,37 @@
+/**
+1. Create Reducers for dynamic data
+2. Create store from Reducers and Provide store to child components in app.js
+3. Create a Thunk action generator to initialize store with Firbase data, and call in app.js
+4. Call action generators to interact with Redux store / Call Thunk action generators to interact with Firebase data and then update the Redux store
+5. Create Selectors to filter Redux store data for desired values ie get only data within a time range
+**/
+
 import React from 'react'
 import ReactDOM from 'react-dom'
+// Used to Provide Redux store to all child components
+import { Provider } from 'react-redux'
+// Passed into Provider, so all children can access store
 import AppRouter from './routers/AppRouter.js'
+
+// Get initial store
+import configureStore from './store/configureStore'
+// Fetches Firebase data for inital store state
+import { startSetScheduleCards } from './actions/scheduleCards'
 
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-ReactDOM.render(<AppRouter/>, document.getElementById('app'))
+const store = configureStore()
+const jsx = (
+  <Provider store = {store}>
+    <AppRouter/>
+  </Provider>
+)
+
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'))
+
+store.dispatch(startSetScheduleCards())
+  .then(() => {
+    console.log(store.getState());
+    ReactDOM.render(jsx, document.getElementById('app'))
+  })
